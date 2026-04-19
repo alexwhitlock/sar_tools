b44dc1da434d86dccf894c6a4159948a1ff303c4
alexwhitlock <alexmwhitlock@gmail.com>
2026-04-19 09:51:32 -0400

    more debugging d4h ref, trying "ref"

--- routes/d4h.py
+++ routes/d4h.py
@@ -86,7 +86,7 @@ def get_d4h_attending_members(activity_id):
             out.append({
                 "name": name or None,
                 "d4hRef": str(mid),
-                "memberRef": m.get("id"),
+                "memberRef": m.get("ref"),
             })
 
         return jsonify({
